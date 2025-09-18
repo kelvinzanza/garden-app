00ab836e4da486836104522fecb006afc125e8ef
kelvin <zanza22zanza@hotmail.com>
2025-09-18 10:57:58 +0200

    Added functions and documentation

--- garden_advice.py
+++ garden_advice.py
@@ -1,3 +1,9 @@
+# garden_advice.py
+
+# TODO: Add functions for cleaner code and reusability (to be done in Issue 2).
+# TODO: Add documentation and comments (to be done in Issue 2).
+
+# Advice dictionary to avoid hardcoding
 advice_dict = {
     "summer": {
         "flower": "Water daily and provide shade in extreme heat.",
@@ -9,10 +15,11 @@ advice_dict = {
     }
 }
 
-
+# Example inputs (inputs can be used later in code)
 season = "summer"
 plant_type = "flower"
 
+# Get advice from dictionary
 if season in advice_dict and plant_type in advice_dict[season]:
     print(advice_dict[season][plant_type])
 else:
